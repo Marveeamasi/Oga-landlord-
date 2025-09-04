3cc757abc7fa8e0bc67c9327fd411155b794b902
Marveeamasi <amasimarvellous@gmail.com>
2025-09-04 15:16:47 +0100

    updwtes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker registered"))
+    .catch((err) => console.log("SW registration failed:", err));
+}
+
 // Dummy database
         const dummyDb = {
             users: [
